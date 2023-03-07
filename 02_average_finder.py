818b55d3079dc599561b2e0b89833ad23a83c535
BishanKD <114677058+BishanKD@users.noreply.github.com>
2023-03-07 19:37:18 +0530

    Update 02_average_finder.py

--- 02_average_finder.py
+++ 02_average_finder.py
@@ -1,11 +1,9 @@
 def average(numbers):
     return sum(numbers)/ len(numbers)
 
-
 count = int(input("Enter number of values: "))
 list = []
 for i in range(count):
     value = int(input("Enter number: "))
     list.append(value)
-
 print(f"The average is {average(list)}")
