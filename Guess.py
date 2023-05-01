fee4bf90da019723e5928691b4b806b2aa7acf0e
Bishan Kumar Deka <114677058+BishanKD@users.noreply.github.com>
2023-05-02 03:47:17 +0530

    Update Guess.py

--- Guess.py
+++ Guess.py
@@ -1,4 +1,7 @@
-target = 34
+import random 
+
+target = random.randint(1,100)
+print("Number is between 1-100")
 guess = int(input("Guess the number: "))
 attempts = 1
 while(guess!=target): 
@@ -8,4 +11,4 @@ while(guess!=target):
     else:
         guess = int(input("Guess higher: "))
         attempts+=1
-print(f"Correct. Took {attempts} attempts. Skill issue\n")
+print(f"Correct. Took {attempts} attempts")
